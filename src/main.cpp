b179f56b9365934c3a674fbc0b2362a889893f55
Nathaniel Bock <bock.nathaniel@gmail.com>
2024-02-26 17:45:06 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#if defined (USBCON) && defined(USBD_USE_CDC)
+#include "USBSerial.h"
+USBSerial usb_serial;
+#endif
+
+void setup()
+{
+	#if defined (USBCON) && defined(USBD_USE_CDC)
+	usb_serial.begin(9600);
+	#endif
+}
+
+void loop()
+{	
+	
+}
